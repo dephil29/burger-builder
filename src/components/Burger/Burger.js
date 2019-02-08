3bdca5721f29ae33cb746d2bcb68293ed2e07c97
Eric Phillips <dphillips@fvi.edu>
2019-02-08 17:13:48 -0500

    added function to map over state object for dynamic burger. 8-153

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,13 +4,21 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = props => {
+  // a way to transform an object of key value pairs into an array of ingredients where the key is the ingredient and the value is the quantity of the ingredient
+  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+    return [...Array(props.ingredients[igKey])].map((_, i) => {
+      return <BurgerIngredient
+        key={igKey + i}
+        type={igKey}
+      />
+    });
+  });
   return (
     <div
       className={classes.Burger}
     >
     <BurgerIngredient type="bread-top" />
-    <BurgerIngredient type="cheese" />
-    <BurgerIngredient type="meat" />
+    {transformedIngredients}
     <BurgerIngredient type="bread-bottom" />
     </div>
   );

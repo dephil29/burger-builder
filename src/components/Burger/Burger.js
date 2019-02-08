e82dcc9f11e266f4dd827a0eb24e00fe76123568
Eric Phillips <dphillips@fvi.edu>
2019-02-08 17:31:03 -0500

    added logic to check if ingredient list of burger is empty and display a message if it is. 8-154

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -5,14 +5,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
 
 const burger = props => {
   // a way to transform an object of key value pairs into an array of ingredients where the key is the ingredient and the value is the quantity of the ingredient
-  const transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
     return [...Array(props.ingredients[igKey])].map((_, i) => {
       return <BurgerIngredient
         key={igKey + i}
         type={igKey}
       />
     });
-  });
+  }).reduce((arr, el) => {
+    return arr.concat(el)
+  }, []);
+  if(transformedIngredients.length === 0){
+    transformedIngredients = <p>Please start adding ingredients!</p>
+  }
+  console.log(transformedIngredients)
   return (
     <div
       className={classes.Burger}

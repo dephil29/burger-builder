67d2334264fe00e883a09572ded42da95f1560ea
Eric Phillips <dphillips@fvi.edu>
2019-02-15 17:58:50 -0500

    fixed select not sending fastest value to firebase

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -57,7 +57,7 @@ class ContactData extends Component {
             {value: 'cheapest', displayValue: 'Cheapest'},        
           ]
         },
-        value: ''
+        value: 'fastest'
       }
     },
     loading: false

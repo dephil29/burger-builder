0502a24728f2aff68d607bf2580987ca4e3880a8
Eric Phillips <dphillips@fvi.edu>
2019-02-19 17:09:54 -0500

    fixed problem with ingredients and price not being sent to firebaseand price on OrderSummary

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -23,7 +23,7 @@ class OrderSummary extends Component {
         <ul>
           {ingredientSummary}
         </ul>
-        <p><strong>Total Price: {this.props.price}</strong></p>
+        <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
         <p>Continue to Checkout?</p>
         <Button
           btnType="Danger"

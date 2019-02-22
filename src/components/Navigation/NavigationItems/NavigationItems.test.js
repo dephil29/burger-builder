1432abe085f8326f7e70a2e9c8cf3292a83a4ead
Eric Phillips <dphillips@fvi.edu>
2019-02-22 17:48:12 -0500

    another test

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,5 +21,10 @@ describe('<NavigationItems />', () => {
   it('should render three <NavigationItem /> elements if authenticated', () => {
     wrapper.setProps({isAuthenticated: true})
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });   
+
+  it('should render three <NavigationItem /> elements if authenticated', () => {
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
   });
 });
